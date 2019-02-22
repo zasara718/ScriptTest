10c237c632b2acc285da46832339241d2cbe6055
zasara <zasara718@gmail.com>
2019-02-22 14:11:32 +0900

    Lesson4課題の修正
    Magic関数を11回呼び出すように修正しました。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -8,18 +8,17 @@ public class Boss {
 	//魔力用の関数
 	public void Magic() {
 
-		for (int m = 1; m <= 11; m ++) {
-			if (m <= 10) {
-				//残りmpを5減らす
-				this.mp -= 5;
-				//減らした数値分の魔力を表示
-				Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");	
+		if (mp >= 5) {
+			//残りmpを5減らす
+			this.mp -= 5;
+			//減らした数値分の魔力を表示
+			Debug.Log("魔法攻撃をした。残りMPは" + this.mp + "。");	
 
-			} else {
-				//11回目の時
-				Debug.Log("MPが足りないため魔法が使えない");
-			}
+		} else {
+			//11回目の時
+			Debug.Log("MPが足りないため魔法が使えない");
 		}
+		
 	}
 }
 
@@ -33,20 +32,21 @@ public class Test : MonoBehaviour {
 		//配列の要素を順番に繰り返す
 		for (int i = 0; i < array.Length; i++) {
 			//配列の要素を表示する
-			Debug.Log(array [i]);
+			//Debug.Log(array [i]);
 		}
 
 		//配列の要素を逆順に繰り返す
 		for (int i = array.Length - 1; i >= 0; i--) {
 			//配列の要素を表示する
-			Debug.Log(array [i]);
+			//Debug.Log(array [i]);
 		}
 
 		//Bossクラスの変数を宣言してインスタンスを代入
-		Boss lastboss = new Boss(); {
-				lastboss.Magic();
+		Boss lastboss = new Boss(); 
+		//Magic関数を１１回繰り返す
+		for (int m = 1; m <= 11; m++) {
+			lastboss.Magic();
 		}
-		
 	}
 	
 	// Update is called once per frame
